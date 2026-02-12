b2ed6bd7b3cb0d8b65a589881ee02e19b42b8657
arshiyahera <arshiya.hera@gmail.com>
2026-02-12 15:42:48 +0600

    Update Entity.cpp
    mainly in the previous one  generateUniqueId(); didnot exist which would be needed to identify future entities

--- Entity.cpp
+++ Entity.cpp
@@ -1,31 +1,25 @@
-#include "Entity.h"
-#include <iostream>
+#ifndef ENTITY_H
+#define ENTITY_H
 
+#include <string>
 using namespace std;
 
-Entity::Entity(const string& entityName)
-    : name(entityName), isActive(true) {
-    id = generateUniqueId();
-    cout << "Entity created: " << name << " (ID: " << id << ")" << endl;
-}
+class Entity {
+private:
+    string id;
+    string name;
+    bool isActive;
+    
+    static string generateUniqueId();
+    
+public:
+    Entity(const string& entityName);
+    
+    string getId() const;
+    string getName() const;
+    bool getIsActive() const;
+    
+    void setIsActive(bool active);
+};
 
-string Entity::getId() const {
-    return id;
-}
-
-string Entity::getName() const {
-    return name;
-}
-
-bool Entity::getIsActive() const {
-    return isActive;
-}
-
-void Entity::setIsActive(bool active) {
-    isActive = active;
-}
-
-string Entity::generateUniqueId() {
-    static int counter = 0;
-    return "E" + to_string(counter++);
-}
+#endif
