3c55d351bf6e569d503581bfbd0513b565bf0d63
Musfirat <farihamusfirat@gmail.com>
2026-03-08 04:01:10 +0600

    Refactor SpatialEntity constructor parameters
    Refactor SpatialEntity constructor to use new parameter names and initialization.

--- SpatialEntity.cpp
+++ SpatialEntity.cpp
@@ -1,32 +1,4 @@
 #include "SpatialEntity.h"
-<<<<<<< HEAD
 
-SpatialEntity::SpatialEntity(const std::string& name, float startX, float startY)
-    : Entity(name), x(startX), y(startY), prevX(startX), prevY(startY)
-{}
-=======
-#include <iostream>
-
-using namespace std;
-
-SpatialEntity::SpatialEntity(const string& name, float startX, float startY)
-    : Entity(name)
-{
-    setPosition(startX, startY);
-}
-
-float SpatialEntity::getX() const { return x; }
-float SpatialEntity::getY() const { return y; }
-void SpatialEntity::setPosition(float newX, float newY) { Entity::setPosition(newX, newY); }
-
-void SpatialEntity::update(float deltaTime) {
-    // Default spatial entity movement using velocity
-    x += vx * deltaTime;
-    y += vy * deltaTime;
-}
-
-void SpatialEntity::draw() {
-    cout << "Drawing SpatialEntity: " << name 
-         << " at (" << x << ", " << y << ")" << endl;
-}
->>>>>>> 29b56c1b6970270daf01c4c6439b989cf6a1884b
+SpatialEntity::SpatialEntity(const std::string& name, float start_x, float start_y)
+     :  Entity(name), x(start_x), y(start_y), prev_x(start_x), prev_y(start_y){}
